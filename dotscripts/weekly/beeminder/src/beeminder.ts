8ba1493fc479f6c9604c4b5f3e396575969e437f
Ozan Öğreden <oguzhan@oguzhanogreden.com>
2021-12-08 21:38:16 +0100

    refactor client wrapper

--- dotscripts/weekly/beeminder/src/beeminder.ts
+++ dotscripts/weekly/beeminder/src/beeminder.ts
@@ -1,40 +1,15 @@
-import { AsyncSubject, map, ReplaySubject, Subject } from "rxjs";
-
 // @ts-nocheck
 var beeminder = require("beeminder");
-
-const TOKEN = process.env.BEEMINDER_TOKEN;
-if (!TOKEN) {
-  throw new Error("BEEMINDER_TOKEN is not set.")
-}
-var bm = beeminder(TOKEN);
 
 // USER DATA
 export type User = {
   goals: string[];
 };
 
-const _userDataStream = new AsyncSubject<User>();
-function getGoalNames() {
-  bm.getUser(function (err: any, result: User) {
-    if (err) {
-      throw new Error("While getting data.");
-    }
-
-    _userDataStream.next(result);
-    _userDataStream.complete();
-  });
-}
-
-export const userDataStream$ = _userDataStream.asObservable();
-export const userDataRequested$ = new Subject<null>();
-
-userDataRequested$.subscribe((_) => getGoalNames());
-
 // GOAL DATA
 export type GoalSlug = string;
 export type GoalRateUnits = "y" | "m" | "w" | "d" | "h";
-type GoalResponse = {
+export type GoalResponse = {
   slug: GoalSlug;
   rate: number;
   runits: GoalRateUnits;
@@ -52,40 +27,3 @@ export type Goal = {
   rate: GoalRate;
   title: string;
 };
-
-let _goalDataStream = new ReplaySubject<GoalResponse>();
-function getGoal(goalName: GoalSlug) {
-  bm.getGoal(goalName, function (err: any, result: GoalResponse) {
-    if (err) {
-      throw new Error(`Can't get goal ${goalName}`);
-    }
-
-    _goalDataStream.next(result);
-  });
-}
-
-export const goalDataRequested$ = new Subject<GoalSlug>();
-export const goalDataStream$ = _goalDataStream.pipe(
-  map((response) => {
-    // if (response.slug === 'beard') {
-    //   console.log(response)
-    // }
-    // if (response.slug === 'reading') {
-    //   console.log(response)
-    // }
-
-    // console.log
-
-    return {
-      rate: {
-        value: response.rate,
-        unit: response.runits,
-        gunit: response.gunits,
-      },
-      slug: response.slug,
-      title: response.title,
-    } as Goal;
-  })
-);
-
-goalDataRequested$.subscribe((goalName) => getGoal(goalName));
---
+++ dotscripts/weekly/beeminder/src/client-wrapper.ts
@@ -0,0 +1,62 @@
+import { GoalSlug } from "beeminder-weekly/beeminder";
+import { map, ReplaySubject, shareReplay, Subject } from "rxjs";
+import { Goal, GoalResponse, User } from "./beeminder";
+
+var _beeminder = require("beeminder");
+
+type Callback<T> = (err: any, result: T) => void;
+type IClient = {
+  getUser: (cb: Callback<User>) => void;
+  getGoal: (goalName: string, cb: Callback<GoalResponse>) => void;
+};
+
+export class Client {
+  private _client: IClient;
+  private _userDataStream = new Subject<User>();
+  private _goalDataStream = new ReplaySubject<GoalResponse>();
+
+  userDataStream$ = this._userDataStream.pipe(shareReplay(1));
+  goalDataStream$ = this._goalDataStream.pipe(
+    map((response) => {
+      return {
+        rate: {
+          value: response.rate,
+          unit: response.runits,
+          gunit: response.gunits,
+        },
+        slug: response.slug,
+        title: response.title,
+      } as Goal;
+    })
+  );
+
+  constructor(token: string) {
+    this._client = _beeminder(token);
+  }
+
+  getGoalNames() {
+    this._client.getUser(this.getGoalNamesCallback);
+  }
+
+  getGoalData(slug: GoalSlug) {
+    this._client.getGoal(slug, this.getGoalDataCallback);
+  }
+
+  private getGoalDataCallback(err: any, result: GoalResponse) {
+    // TODO: Should be in the reactive code?
+    if (err) {
+      throw new Error("Can't get goal.");
+    }
+
+    this._goalDataStream.next(result);
+  }
+
+  private getGoalNamesCallback(err: any, result: User) {
+    // TODO: Should be in the reactive code?
+    if (err) {
+      throw new Error("While getting data.");
+    }
+
+    this._userDataStream.next(result);
+  }
+}
